64d7bf3ba43635a643e64ec2860eb8f259f86feb
aahmetyildiz <yildizaahmet@gmail.com>
2020-02-25 01:36:51 +0100

    Create mysql_connection.py

---
+++ mysql_connection.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+
+cnx_db = mysql.connector.connect(
+    user='python',
+    password='Aa1234',
+    host='localhost',
+    database='proxy',
+    port=3306,
+    charset='utf8')
+
+
+
+
+																						
